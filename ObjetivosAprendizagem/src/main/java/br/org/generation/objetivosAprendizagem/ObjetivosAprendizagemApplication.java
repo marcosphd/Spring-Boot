8ddfe931a58317dccf326fede17e90c2c7b13f66
unknown <md6907179@gmail.com>
2021-05-25 19:08:24 -0300

    Subindo Exercicios

---
+++ ObjetivosAprendizagem/src/main/java/br/org/generation/objetivosAprendizagem/ObjetivosAprendizagemApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.objetivosAprendizagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjetivosAprendizagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjetivosAprendizagemApplication.class, args);
+	}
+
+}
---
+++ helloWord/src/main/java/br/org/generation/hello/controller/HelloController.java
@@ -0,0 +1,13 @@
+package br.org.generation.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //Annotation que indica que é uma classe controladora
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hellow World!!";	
+    }
+}
